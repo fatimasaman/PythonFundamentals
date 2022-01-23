c0c258833363ffd0246e418f72486408bcac3f6c
Saman Fatima <samanfatima@Samans-MBP.fios-router.home>
2022-01-22 19:25:12 -0500

    initial commit

---
+++ functions.py
@@ -0,0 +1,5 @@
+def sheldon_knock (name,number_of_times = 3):  # The default value is 3 it is optional
+    for i in range (number_of_times):
+        print("knock knock knock {}".format(name))
+
+sheldon_knock("penny",9)
---
+++ return_function.py
@@ -0,0 +1,11 @@
+#def add(a,b):
+#   return a+b
+
+#x= add(1,2)
+#print (x)
+
+def div(c,d):
+   return c/d
+
+div(10,2)
+
